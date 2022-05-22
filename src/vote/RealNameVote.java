3b4b748fdba695f6acef4c2db3f73732552f3366
Wang Zhongjie <rainy.wang@gmail.com>
2022-05-22 20:24:10 +0800

    initial version

---
+++ src/vote/RealNameVote.java
@@ -0,0 +1,18 @@
+package vote;
+
+import auxiliary.Voter;
+
+//immutable
+public class RealNameVote<C> extends Vote<C>{
+	
+	//Õ∂∆±»À
+	private Voter voter;
+	
+	public RealNameVote(/*TODO*/) {
+		// TODO
+	}
+	
+	public Voter getVoter() {
+		return this.voter;
+	}
+}
